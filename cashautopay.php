2198df43a12f80afe0ec88417368ec9423ca2cab
ortiz <ortiz@systopia.de>
2025-08-28 14:28:51 +0200

    civix: scaffold de.systopia.cashautopay

---
+++ cashautopay.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once 'cashautopay.civix.php';
+// phpcs:disable
+use CRM_Cashautopay_ExtensionUtil as E;
+// phpcs:enable
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function cashautopay_civicrm_config(&$config): void {
+  _cashautopay_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function cashautopay_civicrm_install(): void {
+  _cashautopay_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function cashautopay_civicrm_enable(): void {
+  _cashautopay_civix_civicrm_enable();
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ */
+//function cashautopay_civicrm_preProcess($formName, &$form): void {
+//
+//}
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
+ */
+//function cashautopay_civicrm_navigationMenu(&$menu): void {
+//  _cashautopay_civix_insert_navigation_menu($menu, 'Mailings', [
+//    'label' => E::ts('New subliminal message'),
+//    'name' => 'mailing_subliminal_message',
+//    'url' => 'civicrm/mailing/subliminal',
+//    'permission' => 'access CiviMail',
+//    'operator' => 'OR',
+//    'separator' => 0,
+//  ]);
+//  _cashautopay_civix_navigationMenu($menu);
+//}
